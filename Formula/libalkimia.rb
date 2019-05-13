00ecc3eb4632edc767fe5e6484d83b9e589e9f6d
sl1pkn07 <sl1pkn07@gmail.com>
2019-05-13 19:36:09 +0200

    Update libalkimia.rb
    update dependencies

--- Formula/libalkimia.rb
+++ Formula/libalkimia.rb
@@ -9,16 +9,15 @@ class Libalkimia < Formula
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
+  depends_on "KDE-mac/kde/kf5-kdesignerplugin" => :build
+  depends_on "KDE-mac/kde/kf5-kpackage" => :build
+  depends_on "KDE-mac/kde/kf5-plasma-framework" => :build
   depends_on "ninja" => :build
-  depends_on "pkgconfig" => :build
+  depends_on "pkg-config" => :build
 
   depends_on "gmp"
-  depends_on "KDE-mac/kde/kf5-kconfig"
-  depends_on "KDE-mac/kde/kf5-kcoreaddons"
   depends_on "KDE-mac/kde/kf5-kdelibs4support"
   depends_on "KDE-mac/kde/kf5-knewstuff"
-  depends_on "KDE-mac/kde/kf5-kpackage"
-  depends_on "KDE-mac/kde/kf5-plasma-framework"
   depends_on "KDE-mac/kde/qt-webkit"
   depends_on "mpir"
 
@@ -36,7 +35,7 @@ class Libalkimia < Formula
       system "ninja", "install"
       prefix.install "install_manifest.txt"
     end
-# Extract Qt plugin and QML2 path
+    # Extract Qt plugin and QML2 path
     mkdir "getqmlpath" do
       (Pathname.pwd/"main.cpp").write <<~EOS
         #include <QTextStream>
